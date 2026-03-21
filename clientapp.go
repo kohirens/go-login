7c48d95a589cb6767f05aabfc6aa38d2273b73a0
Khalifah Shabazz <619281+b01@users.noreply.github.com>
2026-03-21 19:14:28 -0400

    rmv: User Agent Data from Client App
    There seems to be no advantage, security wise, to track the user agent.
    For that reason it is not removed. This will require code changes.
    
    BREAKING CHANGE: Remove User Agent from ClientApp structure.

--- clientapp.go
+++ clientapp.go
@@ -1,6 +1,8 @@
 package login
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 )
 
@@ -8,15 +10,26 @@ import (
 // such as a browser or other application that can view web applications.
 type ClientApp struct {
 	Id        string    `json:"id"`
-	UserAgent string    `json:"userAgent"`
 	AccountId string    `json:"accountId"`
 	LastDate  time.Time `json:"lastDate"`
 }
 
-func NewClientApp(userAgent string) *ClientApp {
+func LoadClientApp(ec []byte) (*ClientApp, error) {
+	if len(ec) == 0 {
+		return NewClientApp(), nil
+	}
+
+	var ca *ClientApp
+	if e1 := json.Unmarshal(ec, &ca); e1 != nil {
+		return nil, fmt.Errorf(stderr.DecodeJSON, e1.Error())
+	}
+
+	return ca, nil
+}
+
+func NewClientApp() *ClientApp {
 	return &ClientApp{
-		Id:        generateId(),
-		UserAgent: userAgent,
-		LastDate:  time.Now().UTC(),
+		Id:       generateId(),
+		LastDate: time.Now().UTC(),
 	}
 }
